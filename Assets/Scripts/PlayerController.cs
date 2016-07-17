1132f86862954fcf5e76a6d7b3ec10e5011155f4
Christopher Casasola <iamchiiyousee@yahoo.com>
2016-07-17 19:03:23 -0400

    starting block

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public Rigidbody rb;
+    public float speed;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        /* float moveHorizontal = Input.GetAxis("Horizontal");
+         float moveVertical = Input.GetAxis("Vertical");
+
+         Vector3 force = new Vector3(moveHorizontal, 0, moveVertical);
+
+         rb.AddForce(force * speed);*/
+
+        //Forward Movement
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * Speed * Time.deltaTime);
+        }
+        //Backward Movement
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.back * (Speed - 2) * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * Speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * Speed * Time.deltaTime);
+        }
+
+        if (Input.GetKeyDown("space"))
+        {
+            Vector3 up = transform.TransformDirection(Vector3.up);
+
+            rb.AddForce(up * 5, ForceMode.Impulse);
+        }
+    }
+}
